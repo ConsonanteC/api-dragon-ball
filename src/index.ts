b2c15c37d3657b42e9c4b2487299af5119a15034
ConsonanteC <pipejimenez.jfjr@gmail.com>
2024-07-05 09:22:59 -0500

    commit3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import {IResponseDB, Item} from "./interfaces/response-db";
 
+const ul = document.querySelector(".ul") as HTMLUListElement;
+
 document.addEventListener("DOMContentLoaded", async () => {
     const data: Item[] = await getResponse;
 
@@ -7,22 +9,40 @@ document.addEventListener("DOMContentLoaded", async () => {
         console.log(character);
 
         const li = document.createElement("li") as HTMLLIElement;
+        const name = document.createElement("h4") as HTMLHeadingElement; 
+        const image = document.createElement("img") as HTMLImageElement;
+        const ki = document.createElement("p") as HTMLParagraphElement;
+        const maxKi = document.createElement("p") as HTMLParagraphElement;
+        const race = document.createElement("p") as HTMLParagraphElement;
+        const gender = document.createElement("p") as HTMLParagraphElement;
+        const description = document.createElement("p") as HTMLParagraphElement;
+        const affiliation = document.createElement("p") as HTMLParagraphElement;
+        const deleteAt = document.createElement("deleteAt") as HTMLInputElement;
 
+        name.innerText = character.name;
+        image.src = character.image;
+        image.className = "image";
+        ki.innerText = `Ki: ${character.ki}`;
+        maxKi.innerText = `Max Ki: ${character.maxKi}`;
+        race.innerText = `Race: ${character.race}`;
+        gender.innerText = `Gender: ${character.gender}`;
+        description.innerText = `Description: ${character.description}`;
+        affiliation.innerText = `Affiliation: ${character.affiliation}`;
+        deleteAt.type = "checkbox";
 
+        li.appendChild(name);
+        li.appendChild(image);
+        li.appendChild(ki);
+        li.appendChild(maxKi);
+        li.appendChild(race);
+        li.appendChild(gender);
+        li.appendChild(description);
+        li.appendChild(affiliation);
+        li.appendChild(deleteAt);
 
-    //     const li = document.createElement("li") as HTMLLIElement;
-    // const name = document.createElement("h4") as HTMLHeadingElement;
-    // const image = document.createElement("img") as HTMLImageElement;
-    // const status= document.createElement("p") as HTMLParagraphElement;
-    // const specie= document.createElement("p") as HTMLParagraphElement;
-    // const gender= document.createElement("p") as HTMLParagraphElement;
-    // const location= document.createElement("p") as HTMLParagraphElement;
-    // const origin= document.createElement("p") as HTMLParagraphElement;
+        ul.appendChild(li);
     })
 });
-
-
-
 
 async function getResponse (): Promise<Item[]> {
     const response = await fetch ("https://dragonball-api.com/api/characters");
